e6a2dbce4cd831b9af9b5d40f91d3255cd2ec0b9
SilentWolf6662 <52424766+SilentWolf6662@users.noreply.github.com>
2022-08-29 14:43:19 +0200

    CustomArduino2Unity (Delay/Latency not fixed)
    This approach is a bit weird but it works and is doing it's work (Delay/Latency not fixed)

---
+++ CustomArduino2Unity/A2U_Right.cs
@@ -0,0 +1,31 @@
+﻿#region Copyright Notice
+// ******************************************************************************************************************
+// 
+// Frogger (Clone).Assembly-CSharp.A2U_Up.cs © SilentWolf6662 - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+// 
+// This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+// To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/
+// 
+// Created & Copyrighted @ 2022-08-29
+// 
+// ******************************************************************************************************************
+#endregion
+using UnityEngine;
+namespace Froggy
+{
+	public class A2U_Right : MonoBehaviour
+	{
+		[SerializeField] private Arduino2Unity a2u;
+		private void Update()
+		{
+			if (a2u.sp.IsOpen)
+				try
+				{
+					if (a2u.sp.ReadByte() == 3) a2u.player.rb.MovePosition(a2u.player.rb.position + Vector2.right);
+				}
+				catch (System.Exception) { /* ignored */ }
+		}
+	}
+}
---
+++ CustomArduino2Unity/Arduino2Unity.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.IO.Ports;
+
+// make sure to name your C# file the same as your class name. In this case Move.cs 
+
+namespace Froggy
+{
+	public class Arduino2Unity : MonoBehaviour
+	{
+		[SerializeField] public Player player;
+		private const string port = "COM3";
+		// change your serial port
+		public readonly SerialPort sp = new SerialPort($"\\\\.\\{port}", 38400);
+
+		private void OnEnable()
+		{
+			sp.Open();
+			sp.ReadTimeout = 100;
+		}
+
+		private void OnDisable() => sp.Close();
+	}
+}
